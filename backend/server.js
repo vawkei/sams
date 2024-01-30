0051d9a3d1f107315d7dfd53c7fdefe9a6fa7fc3
voke sokoh <vokes9810@gmail.com>
2024-01-30 05:13:21 +0100

    added MongoDBsTORE and changed the sessions secret in server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const fileUpload = require ("express-fileupload");
 const cloudinary = require("cloudinary").v2;
@@ -34,11 +35,17 @@ app.use(cors({
 app.use(cookieParser())
 
 
+const store = new MongoDBStore({
+    uri: process.env.MONGO_URI,
+    collection: 'sessions',
+  })
+
 const session = require("express-session")
 app.use(session({
- secret: process.env.PAYSTACK_TEST_SECRET_KEY,
+ secret: process.env.EXPRESS_SESSION_SECRET,
  resave: false,
  saveUninitialized: true,
+ store:store,
  cookie: { secure: false } // Set secure to true if you're using HTTPS
 }));
 
@@ -77,8 +84,11 @@ const port = process.env.PORT || 5001;
 
 const start =async ()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URI);
-        app.listen(port,"localhost",()=>{
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+          });
+        app.listen(port,'0.0.0.0',()=>{
             console.log("it's on");
             console.log("connected to DB");
             console.log(`Server listening on port ${port}`)
@@ -89,3 +99,118 @@ const start =async ()=>{
 };
 
 start();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//======== This is the one before deployment: Use this in DEVELOPMENT MODE=======//
+
+// require("dotenv").config();
+// require("express-async-errors");
+
+
+// const express = require("express");
+// const app = express();
+
+// const cors = require("cors");
+// const mongoose = require("mongoose");
+// const cookieParser = require("cookie-parser");
+
+// const fileUpload = require ("express-fileupload");
+// const cloudinary = require("cloudinary").v2;
+
+// // rest of the packages
+
+// const errorMiddleware = require("./middlewares/error-handler-middleware");
+
+// const userRoute = require("./routes/userRoutes");
+// const productRoute = require("./routes/productRoutes");
+// const categoryRoute = require("./routes/categoryRoutes"); 
+// const couponRoute = require("./routes/couponRoutes");
+// const orderRoute = require("./routes/orderRoutes");
+// const paystackRoute = require("./routes/paystackRoutes");
+
+// app.use(express.urlencoded({extended:false}));
+
+// app.use(cors({
+//     origin:["http://localhost:3001","https://samsapp.onrender.com"],
+//     credentials:true
+// }));
+
+
+// app.use(cookieParser())
+
+
+// const session = require("express-session")
+// app.use(session({
+//  secret: process.env.PAYSTACK_TEST_SECRET_KEY,
+//  resave: false,
+//  saveUninitialized: true,
+//  cookie: { secure: false } // Set secure to true if you're using HTTPS
+// }));
+
+// app.use("/api/v1/paystack",paystackRoute);
+// //note: we put the paystackRoute above the express.json() cuz we dont want the express.json() applied to it, since we will be using a webhook for the paystackRoute.
+             
+
+// app.use(express.json());
+
+// app.use(fileUpload({useTempFiles:true}))
+// cloudinary.config({
+//     cloud_name:process.env.CLOUDINARY_NAME,
+//     api_key:process.env.CLOUDINARY_API_KEY,
+//     api_secret:process.env.CLOUDINARY_API_SECRET
+// });
+
+
+// //Routes:
+// app.get("/",(req,res)=>{
+//     res.send("<h1>We live Baby</h1>")
+// });
+// app.use("/api/v1/auth",userRoute);
+// app.use("/api/v1/products",productRoute);
+// app.use("/api/v1/categories",categoryRoute);
+// app.use("/api/v1/coupons",couponRoute);
+// app.use("/api/v1/orders",orderRoute);
+
+// app.use(errorMiddleware);
+
+
+// const port = process.env.PORT || 5001;
+// // app.listen(port,"localhost",()=>{
+// //     console.log("it's on");
+// //     console.log(`Server listening on port ${port}`);
+// // });
+
+// const start =async ()=>{
+//     try{
+//         await mongoose.connect(process.env.MONGO_URI);
+//         app.listen(port,"localhost",()=>{
+//             console.log("it's on");
+//             console.log("connected to DB");
+//             console.log(`Server listening on port ${port}`)
+//         })
+//     }catch(error){
+//         console.log(error)
+//     };
+// };
+
+// start();
