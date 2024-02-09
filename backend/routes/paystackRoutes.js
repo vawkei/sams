6f7f90109311b2928240255f9ad297cd7ab8c56a
voke sokoh <vokes9810@gmail.com>
2024-02-09 19:53:28 +0100

    still

--- backend/routes/paystackRoutes.js
+++ backend/routes/paystackRoutes.js
@@ -8,7 +8,6 @@
 
 // const {initializePayment,webhook} = require("../controllers/paystackController");
 
-
 // //paystack routes
 // router.post("/acceptpayment", express.json(), initializePayment.acceptPayment);
 
@@ -16,8 +15,6 @@
 // router.post("/verifypayment",express.json(), initializePayment.verifyPayment);
 
 // router.post("/charge",express.json(), initializePayment.initiateCardPayment);
-
-
 
 // router.post(
 //   "/webhook",
@@ -28,28 +25,37 @@
 // module.exports = router;
 //=============================codeA stops here====================================
 
-
 //codeB
 //===================When working with the web socket===========================
-//This allows you to pass the io object to the webhook function when defining the route. 
+//This allows you to pass the io object to the webhook function when defining the route.
 
 const express = require("express");
 const router = express.Router();
-const { initializePayment, webhook } = require("../controllers/paystackController");
+const {
+  initializePayment,
+  webhook,
+} = require("../controllers/paystackController");
 
 module.exports = (io) => {
-    router.post("/acceptpayment", express.json(), initializePayment.acceptPayment);
-    router.post("/verifypayment", express.json(), initializePayment.verifyPayment);
-    router.post("/charge", express.json(), initializePayment.initiateCardPayment);
+  router.post(
+    "/acceptpayment",
+    express.json(),
+    initializePayment.acceptPayment
+  );
+  router.post(
+    "/verifypayment",
+    express.json(),
+    initializePayment.verifyPayment
+  );
+  router.post("/charge", express.json(), initializePayment.initiateCardPayment);
 
-    router.post(
-        "/webhook",
-        express.raw({ type: "application/json" }),
-        webhook(io)
-    );
+  router.post(
+    "/webhook",
+    express.json(), // Use express.json() to parse JSON payloads
+    webhook(io)
+  );
 
-    return router;
+  return router;
 };
-
 
 // ctrl + shift + c is used to copy from cli
