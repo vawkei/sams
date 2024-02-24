94a64dfa8b097a42e57205929089613aaf45f33f
voke sokoh <vokes9810@gmail.com>
2024-02-24 13:19:40 +0100

    another error used live instead of test key

--- backend/controllers/paystackController.js
+++ backend/controllers/paystackController.js
@@ -251,7 +251,7 @@ const crypto = require("crypto");
 const webhook = async (req, res) => {
   try {
     const hash = crypto
-      .createHmac("sha512", process.env.PAYSTACK_LIVE_SECRET_KEY)
+      //.createHmac("sha512", process.env.PAYSTACK_LIVE_SECRET_KEY)
       .createHmac("sha512", process.env.PAYSTACK_TEST_SECRET_KEY)
       .update(JSON.stringify(req.body)) // Stringify the object to create a hash
       .digest("hex");
