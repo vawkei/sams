3fee1efeb2f64d8519c2881a6f081e09399af3b2
voke sokoh <vokes9810@gmail.com>
2024-02-25 11:21:19 +0100

    i think we got it now

--- backend/models/webhooks.js
+++ backend/models/webhooks.js
@@ -1,36 +1,34 @@
 const mongoose = require("mongoose");
 
-const webhookSchema = mongoose.Schema({
-    event:{
-        type:[Object],
-        required:[true,"Webhook needed"]
+const webhookSchema = mongoose.Schema(
+  {
+    event: {
+      type: [Object],
+      required: [true, "Webhook needed"],
     },
-    firstName:{
-        type:String,
-        trim:true,
-        required:[true,"Please insert name"]
+    firstName: {
+      type: String,
+      trim: true,
+      required: [true, "Please insert name"],
     },
-    email:{
-        type:String,
-        required:[true,"enter email"]
+    email: {
+      type: String,
+      required: [true, "enter email"],
     },
-    // createdBy:{
-    //     type:String,
-    //     trim:true,
-    //     required:[true,"Please insert name"]
-    // },
-    cartItems:{
-        type:[Object],
+    cartItems: {
+      type: [Object],
     },
-    timeReceived:{
-        type:Date,
-        required:[true,"enter time received"]
+    dateReceived: {
+      type: Date,
+      required: [true, "enter date received"],
     },
-    dateReceived:{
-        type:Date,
-        required:[true,"enter date received"]
-    }
+    createdBy: {
+      type: String,
+      trim: true,
+      required: [true, "Please insert _id"],
+    },
+  },
+  { timestamps: true }
+);
 
-},{timestamps:true});
-
-module.exports = mongoose.model("webhooks",webhookSchema)
+module.exports = mongoose.model("webhooks", webhookSchema);
